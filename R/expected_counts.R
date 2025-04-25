5bb5dfb92ec2cb8589bc58696d507440be0a542a
vgrozd <85968442+vgrozd@users.noreply.github.com>
2025-04-25 17:45:07 +0200

    Add R/expected_counts.R

---
+++ R/expected_counts.R
@@ -0,0 +1,11 @@
+#' Title
+#'
+#' @param counts
+#'
+#' @returns An integer of expected counts
+#' @export
+#'
+#' @examples expected_counts(Seurat@assays$RNA@counts)
+expected_counts <- function(counts){
+
+}
