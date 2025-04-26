fd31221cb2fa6479771ded7da4c10181f4de8cd2
vgrozd <85968442+vgrozd@users.noreply.github.com>
2025-04-26 23:51:44 +0200

    Update R/expected_counts.R

--- R/expected_counts.R
+++ R/expected_counts.R
@@ -2,12 +2,13 @@
 #'
 #' @param counts A counts matrix
 #' @param factor Integer, a scaling factor
+#' @param log Logical, log counts?
 #'
 #' @returns An integer of expected counts
 #' @export
 #'
 #' @examples expected_counts(Seurat@assays$RNA@counts)
-expected_counts <- function(counts, factor=1e6){
+expected_counts <- function(counts, factor=1e6, log=FALSE){
   return(
     rowSums(counts) * factor / sum(rowSums(counts))
   )
