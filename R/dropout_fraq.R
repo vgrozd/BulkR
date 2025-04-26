a5800d2e0d982d4a546475cf05bc3c37f28e8e58
vgrozd <85968442+vgrozd@users.noreply.github.com>
2025-04-27 00:18:59 +0200

    Update R/dropout_fraq.R

--- R/dropout_fraq.R
+++ R/dropout_fraq.R
@@ -1,3 +1,12 @@
+#' Title
+#'
+#' @param counts Numeric vector, counts of a single cell/sample
+#' @param expected_counts Numeric vector, expected counts per gene/feature in the same order as counts
+#'
+#' @returns
+#' @export
+#'
+#' @examples
 dropout_fraq <- function(counts, expected_counts){
 
 }
