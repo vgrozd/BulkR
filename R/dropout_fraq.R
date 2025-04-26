8099ad84e0fea7b3c84a6d1ad70ca7effcfe7b36
vgrozd <85968442+vgrozd@users.noreply.github.com>
2025-04-27 00:21:01 +0200

    Update R/dropout_fraq.R

--- R/dropout_fraq.R
+++ R/dropout_fraq.R
@@ -3,10 +3,10 @@
 #' @param counts Numeric vector, counts of a single cell/sample
 #' @param expected_counts Numeric vector, expected counts per gene/feature in the same order as counts
 #'
-#' @returns
+#' @returns dropout fraction per expected_counts bin
 #' @export
 #'
-#' @examples
+#' @examples dropout_fraq(Seurat@assays$RNA@counts[1,], expected_counts(Seurat@assays$RNA@counts))
 dropout_fraq <- function(counts, expected_counts){
 
 }
