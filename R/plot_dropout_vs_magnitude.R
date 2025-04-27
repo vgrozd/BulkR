dc807c702cd107c75b0a8ef1b08eb9b23ee788bc
vgrozd <85968442+vgrozd@users.noreply.github.com>
2025-04-28 01:13:51 +0200

    Update R/plot_dropout_vs_magnitude.R

--- R/plot_dropout_vs_magnitude.R
+++ R/plot_dropout_vs_magnitude.R
@@ -38,74 +38,54 @@ plot_dropout_vs_magnitude <- function(
 
   } else {
 
-    apply(
+    newdata = data.frame(
+      expected_counts = seq(
+        from = min(expected_counts, na.rm = TRUE),
+        to = max(expected_counts, na.rm = TRUE),
+        length.out = fit.res
+      )
+    )
+
+    dat <- apply(
       as.matrix(dropout_fraq),
-      FUN = function(x){
+      FUN = function(x, expcs = expected_counts, nwdt = newdata){
         fit <- stats::glm(
-          x ~ .expected_counts,
+          x ~ expcs,
           data = data.frame(
-            expected_counts = rep(
-              .expected_counts,
-              dim(as.matrix(x))[2]
-            ),
+            expected_counts = expcs,
             dropout_fraq = as.vector(x)
           ),
           family = quasibinomial
         )
 
-
-
         return(
-          predict(fit, newdata = data.frame(expected_counts=seq(min(expected_counts), max(expected_counts), length.out = fit.res)), type = "response")
+          predict(
+            fit,
+            nwdt,
+            type = "response"
+          )
         )
 
-      },
-      .expected_counts=expected_counts, .fit.res = fit.res,
-      MARGIN = 2
-    )
+      }, MARGIN = 2)
 
-    fit <- stats::glm(
-      dropout_fraq ~ expected_counts,
-      data = data.frame(
-        expected_counts = rep(
-          expected_counts,
-          dim(as.matrix(dropout_fraq))[2]
-        ),
-        dropout_fraq = as.vector(dropout_fraq)
-      ),
-      family = quasibinomial
-    )
-
-   expected_counts = seq(min(expected_counts), max(expected_counts), length.out = fit.res)
-
-   dropout_fraq <- do.call(
-     cbind,
-     list(apply(
-       as.matrix(expected_counts),
-       FUN = function(x){
-         predict(fit, newdata = data.frame(x), type = "response")
-       },
-       MARGIN = 2
-     )
-   ))
 
     return(
 
       ggplot2::ggplot(
         data.frame(
           expected_counts = rep(
-            newdata$expected_counts,
-            dim(as.matrix(newdata$dropout_fraq))[2]
+            log10(newdata$expected_counts+0.5),
+            dim(as.matrix(dat))[2]
           ),
-          dropout_fraq = as.vector(newdata$dropout_fraq),
+          dropout_fraq = as.vector(dat),
           Cell = rep(
-            paste0("Cell_", 1:dim(as.matrix(newdata$dropout_fraq))[2]),
-            each = dim(as.matrix(newdata$dropout_fraq))[2]
+            paste0("Cell_", 1:dim(as.matrix(dat))[2]),
+            each = length(newdata$expected_counts)
           )
         )
       ) +
         ggplot2::aes(expected_counts, dropout_fraq, fill=Cell) +
-        ggplot2::geom_line() +
+        ggplot2::geom_smooth(..., se = TRUE) +
         ggplot2::theme_classic()
     )
 
