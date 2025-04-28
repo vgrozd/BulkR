0b4963af410d090b85d2c16a99296113d8324eba
vgrozd <85968442+vgrozd@users.noreply.github.com>
2025-04-28 10:49:41 +0200

    Update R/expected_counts.R

--- R/expected_counts.R
+++ R/expected_counts.R
@@ -10,12 +10,12 @@
 #' @examples expected_counts(Seurat@assays$RNA@counts)
 #' @examples expected_counts(Seurat@assays$RNA@counts, factor=1e3, log=TRUE, pseudocount=0.5)
 
-expected_counts <- function(counts, factor=1e6, log=FALSE, pseudocount=NULL){
+expected_counts <- function(counts, factor=1e6, log=FALSE, pseudocount=1){
   return(
-    if(log){
-      (Matrix::rowSums(counts)+if(is.null(pseudocount))1 else pseudocount) * factor / sum(Matrix::rowSums(counts)+if(is.null(pseudocount)) 1 else pseudocount)
+    if(!log){
+      Matrix::rowSums(counts, na.rm = TRUE) * factor / sum(Matrix::rowSums(counts, na.rm = TRUE), na.rm = TRUE)
     } else{
-      Matrix::rowSums(counts) * factor / sum(Matrix::rowSums(counts))
+      (Matrix::rowSums(counts, na.rm = TRUE)+ pseudocount) * factor / sum(Matrix::rowSums(counts, na.rm = TRUE) + pseudocount, na.rm = TRUE)
     }
 
   )
