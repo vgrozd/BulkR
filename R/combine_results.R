3e0dff6ad6cd1de8326f8169558bb67af49bfe96
vgrozd <85968442+vgrozd@users.noreply.github.com>
2025-09-13 14:00:20 +0200

    Update combine_results

--- R/combine_results.R
+++ R/combine_results.R
@@ -1,7 +1,12 @@
 
 
-combine_results <- function(..., fdr=NULL, l2fc=NULL, list=FALSE){
+combine_results <- function(..., filter=TRUE, fdr=NULL, l2fc=NULL, type="list"){
 
+  if(! type %in% c("list", "df", "tbl")){
+    stop(
+      "Unvalid return type. Please specify one of: \"list\", \"df\", \"tbl\""
+    )
+  }
 
   if(is.null(names(list(...)))){
     results_list = setNames(
@@ -15,71 +20,102 @@ combine_results <- function(..., fdr=NULL, l2fc=NULL, list=FALSE){
     )
   }
 
+  if(filter){
 
-  if(is.null(fdr)){
-    message("Using default FDR value qval < 0.05")
-    fdr = 0.05
-  }
-  if(length(fdr)==1){
-    if(length(results_list) > 1){
+    if(is.null(fdr)){
+      message("Using default FDR value qval < 0.05")
+      fdr = 0.05
+    }
+    if(length(fdr)==1){
+      if(length(results_list) > 1){
         message(paste0("Using FDR qval<", fdr, " for all DE results"))
         fdr = rep(fdr, length(results_list))
-    }
-  } else{
-    fdr = fdr[which(!is.na(fdr) & !is.null(fdr) & is.numeric(fdr) & (fdr>0) & (fdr<=1))]
-    if(length(fdr)==length(results_list)){
-      message("Using specified FDR values... ")
-    } else {
-      message(
-        "Specified FDRs not same length as results. Reverting to default FDR value qval < 0.05... "
-      )
-      fdr = rep(0.05, length(results_list))
-    }
-  }
-
-  if(is.null(l2fc)){
-    message("Using default L2FC value <0, >0")
-    l2fc = 0
-  }
-  if(length(l2fc)==1){
-    if(length(results_list) > 1){
-      message(paste0("Using L2FC value ", l2fc, " for all DE results"))
-      l2fc = rep(l2fc, length(results_list))
-    }
-  } else{
-    l2fc = l2fc[which(!is.na(l2fc) & !is.null(l2fc) & is.numeric(l2fc))]
-    if(length(l2fc)==length(results_list)){
-      message("Using specified L2FC values... ")
-    } else {
-      message(
-        "Specified L2FCs not same length as results. Reverting to default L2FC value 0 ... "
-      )
-      l2fc = rep(0.05, length(results_list))
-    }
-  }
-
-  fdr = fdr[!is.null(results_list)]
-  l2fc = l2fc[!is.null(results_list)]
-
-  results_list = results_list[!is.null(results_list)]
-  message("---")
-  tryCatch({
-    nm = "?"
-    res_l <- lapply(
-      seq_along(results_list),
-      function(i){
-        nm <<- names(results_list)[i]
-        significant_results(results_list[[i]], qval = fdr[i], l2fc = l2fc[i])
       }
-    )
-    res_l <- setNames(res_l, nm = names(results_list))
-  }, error=function(e){
-    stop(paste0("Could not extract significant results from DE results ", nm))
-    # TODO check here what happens if a result is null or doesn't have sign results
-  }
-  )
+    } else{
+      fdr = fdr[which(!is.na(fdr) & !is.null(fdr) & is.numeric(fdr) & (fdr>0) & (fdr<=1))]
+      if(length(fdr)==length(results_list)){
+        message("Using specified FDR values... ")
+      } else {
+        message(
+          "Specified FDRs not same length as results. Reverting to default FDR value qval < 0.05... "
+        )
+        fdr = rep(0.05, length(results_list))
+      }
+    }
 
-  if(list) {
+    if(is.null(l2fc)){
+      message("Using default L2FC value <0, >0")
+      l2fc = 0
+    }
+    if(length(l2fc)==1){
+      if(length(results_list) > 1){
+        message(paste0("Using L2FC value ", l2fc, " for all DE results"))
+        l2fc = rep(l2fc, length(results_list))
+      }
+    } else{
+      l2fc = l2fc[which(!is.na(l2fc) & !is.null(l2fc) & is.numeric(l2fc))]
+      if(length(l2fc)==length(results_list)){
+        message("Using specified L2FC values... ")
+      } else {
+        message(
+          "Specified L2FCs not same length as results. Reverting to default L2FC value 0 ... "
+        )
+        l2fc = rep(0.05, length(results_list))
+      }
+    }
+
+    fdr = fdr[!is.null(results_list)]
+    l2fc = l2fc[!is.null(results_list)]
+
+    results_list = results_list[!is.null(results_list)]
+    message("---")
+    tryCatch({
+      nm = "?"
+      res_l <- lapply(
+        seq_along(results_list),
+        function(i){
+          nm <<- names(results_list)[i]
+          significant_results(results_list[[i]], qval = fdr[i], l2fc = l2fc[i])
+        }
+      )
+      res_l <- setNames(res_l, nm = names(results_list))
+    }, error=function(e){
+      stop(paste0(
+        "Could not extract significant results from DE results \"", nm, "\"",
+        "\nDid you mistype an argument '", nm, "'? "
+      ))
+      # TODO check here what happens if a result is null or doesn't have sign results
+    }
+    )
+
+  } else{
+
+    results_list = results_list[!is.null(results_list)]
+    message("---")
+    tryCatch({
+      nm = "?"
+      res_l <- lapply(
+        seq_along(results_list),
+        function(i){
+          nm <<- names(results_list)[i]
+          unify_DE_format(results_list[[i]])
+        }
+      )
+      res_l <- setNames(res_l, nm = names(results_list))
+    }, error=function(e){
+      stop(paste0(
+        "Could not extract unify format of DE results: \"", nm, "\""),
+        ,
+        "\nDid you mistype an argument '", nm, "'? "
+      )
+      # TODO check here what happens if a result is null or doesn't have sign results
+    }
+    )
+
+  }
+
+
+  if(type=="list") {
     return(res_l)
   }
 
@@ -88,6 +124,8 @@ combine_results <- function(..., fdr=NULL, l2fc=NULL, list=FALSE){
 
 
 }
+
+
 
 lapply(
   l5,
