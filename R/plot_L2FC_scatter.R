ca5e31b438b8986692ada719aeb640af59d5eef5
vgrozd <85968442+vgrozd@users.noreply.github.com>
2025-09-13 23:41:31 +0200

    Update R/plot_L2FC_scatter.R

--- R/plot_L2FC_scatter.R
+++ R/plot_L2FC_scatter.R
@@ -8,8 +8,65 @@
 #' @export
 #'
 #' @examples
-plot_L2FC_scatter <- function(DE_Results_comb, x=NULL, y=NULL){
+plot_L2FC_scatter <- function(DE_Results_comb, x=NULL, y=NULL, genes="All", flavor="Min", fdr=0.05, l2fc=0){
 
-  axes = grep("L2FC_", colnames(DE_Results_comb), value=TRUE)[1:2]
-  p1 = ggplot(DE)
+  axes = select_comb_DE_results(DE_Results_comb, X=x, Y=y, type = "L2FC")
+
+  p1 = ggplot2::ggplot() +
+
+    ggplot2::geom_point(
+      ggplot2::aes(
+        x=.data[[axes[1]]],
+        y=.data[[axes[2]]]
+      ),
+      data = DE_Results_comb[
+        which(
+          DE_Results_comb[[stringr::str_replace(axes[1], "L2FC", "QVAL")]] >= fdr |
+            (DE_Results_comb[[stringr::str_replace(axes[1], "L2FC", "QVAL")]] < fdr &
+               abs(DE_Results_comb[[axes[1]]]) <= l2fc)
+        ),
+      ],
+      pch=21,
+      fill="blue"
+    ) +
+
+    ggplot2::geom_point(
+      ggplot2::aes(
+        x=.data[[axes[1]]],
+        y=.data[[axes[2]]]
+      ),
+      data = DE_Results_comb[
+        which(
+          DE_Results_comb[[stringr::str_replace(axes[2], "L2FC", "QVAL")]] >= fdr |
+            (DE_Results_comb[[stringr::str_replace(axes[2], "L2FC", "QVAL")]] < fdr &
+               abs(DE_Results_comb[[axes[2]]]) <= l2fc)
+        ),
+      ],
+      pch=21,
+      fill="green"
+    ) +
+
+    ggplot2::geom_point(
+      ggplot2::aes(
+        x=.data[[axes[1]]],
+        y=.data[[axes[2]]]
+      ),
+      data = DE_Results_comb[
+        which(
+          (DE_Results_comb[[stringr::str_replace(axes[1], "L2FC", "QVAL")]] < fdr &
+               abs(DE_Results_comb[[axes[1]]]) > l2fc) &
+            (DE_Results_comb[[stringr::str_replace(axes[2], "L2FC", "QVAL")]] < fdr &
+               abs(DE_Results_comb[[axes[2]]]) > l2fc)
+
+        ),
+      ],
+      pch=21,
+      fill="red"
+    )
+
+
+  return(p1)
+
+
+
 }
